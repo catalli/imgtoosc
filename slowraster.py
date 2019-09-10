a10b31001ea1781ea8f3fed01a3130387c3ce273
Ali Cataltepe <ali@cataltepe.com>
2019-09-09 21:22:18 -0700

    modified raster-type rendering

--- slowraster.py
+++ slowraster.py
@@ -38,7 +38,7 @@ def write_pcm(f, samples, sampwidth=2, framerate=44100, bufsize=2048):
     # Write samples as raw PCM data."
     max_amplitude = float(int((2 ** (sampwidth * 8)) / 2) - 1)
 
-    # split the samples into chunks (to reduce memory consumption and improve performance)
+    # split the samples into chunks (to reduce memory consumption and improve performance)i
     for chunk in grouper(bufsize, samples):
         frames = b"".join(b"".join(struct.pack('h', int(max_amplitude * sample)) for sample in channels) for channels in chunk if channels is not None)
         f.write(frames)
@@ -56,15 +56,24 @@ xzero = im.size[0]/2
 yzero = im.size[1]/2
 values=[]
 for x in range(0, im.size[0]):
-    for y in range(0,im.size[1]):
+    if x % 2 == 0:
+        yrange = range(0, im.size[1])
+    else:
+        yrange = range(im.size[1]-1,-1,-1)
+    for y in yrange:
         if pix[x,y][3] == 255 and pix[x,y][0]+pix[x,y][1]+pix[x,y][2] <= int(sys.argv[2]):
             values.append((float((x-xzero))/divisor,float(-(y-yzero))/divisor))
+
+revvals = reversed(values)
+
+for v in revvals:
+    values.append(v)
 
 sampleimage = cycle(values)
 
 nchannels=2
 sampwidth=2
-framerate=im.size[0]*im.size[1]*120
+framerate=len(values)*120
 nframes=framerate
 
 samples = islice(sampleimage, nframes)
